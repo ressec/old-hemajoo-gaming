6ab3e95de691c65f578c608eb573221ab66925f2
Resse Christophe <christophe.resse@gmail.com>
2019-02-06 18:45:58 +0100

    Added code entities.

---
+++ hemajoo-gaming-atlas-common/src/main/java/com/hemajoo/gaming/atlas/common/base/Keyable.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright(c) 2018 - Hemajoo Company.
+ * ---------------------------------------------------------------------------
+ * This file is part of the Hemajoo's Atlas Gaming project which is licensed
+ * under the Apache license version 2 and use is subject to license terms.
+ * You should have received a copy of the license with the project's artifact
+ * binaries and/or sources.
+ * 
+ * License can be consulted at http://www.apache.org/licenses/LICENSE-2.0
+ * ---------------------------------------------------------------------------
+ */
+package com.hemajoo.gaming.atlas.common.base;
+
+/**
+ * When a class become keyable it acquires the ability for its instances to be retrieved
+ * by keys.
+ * <hr>
+ * @author  <a href="mailto:christophe.resse@gmail.com">Resse Christophe - Hemajoo</a>
+ * @version 1.0.0
+ */
+public interface Keyable
+{
+	//Empty.
+}
